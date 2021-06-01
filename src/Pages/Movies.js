2b91fb8fad86e2b192c9ba9dbefb951da2b39f90
irinam759 <a4759139@gmail.com>
2021-06-01 12:03:44 +0300

    design thumbnails,add movies gallery

--- src/Pages/Movies.js
+++ src/Pages/Movies.js
@@ -1,73 +1,51 @@
 import React from 'react';
-import { Col, Row } from 'react-bootstrap';
-import MovieThumbnail from '../components/MovieThumbnail';
-import { DISNEY_ID } from '../constants';
-import { TMDBDiscover } from '../utils';
+import MoviesGallery from '../components/MoviesGallery';
+import Paginator from '../components/Paginator-generic';
+import { createMoviesObj, TMDBDiscover } from '../utils';
 
 
 class Movies extends React.Component {
     constructor(props){
         super(props);
         this.state={
-            moviesList:[]
+            moviesList:[],
+            totalPages:1,
+            currentPage:1
 
         }
     }
-    
+    setCurrentPage = (pageNum) =>{
+
+        this.setState({
+            currentPage:pageNum
+        })
+
+    }
     componentDidMount = () =>{
-        let pageNum=1;
-
-        TMDBDiscover({page:pageNum})
-        .then((res)=>{
-            if( res && res.results){
-            const movieObj= res.results.map((movie)=>{
-                
-                return{
-                    title: movie.original_title,
-                    laguage: movie.original_language,
-                    overview: movie.overview,
-                    releaseDate: movie.release_date,
-                    rate:movie.vote_average,
-                     runTime:movie.with_runtime,
-                    total_pages:movie.total_pages,
-                    popularity:movie.popularity,
-                    poster_path: `https://www.themoviedb.org/t/p/w220_and_h330_face/${movie.poster_path}`,
-                      }
-
-               
-                                    })
-
+        TMDBDiscover({page:this.state.currentPage})
+        .then((listOfMovies)=>{
+            if( listOfMovies && listOfMovies.results){
+            const pages=listOfMovies.total_pages;
+            const moviesObj = createMoviesObj(listOfMovies);
+            
                     this.setState({
-                            moviesList:movieObj
+                            moviesList:moviesObj,
+                            totalPages:pages
                             })
                  }
                    
             })
     }
     render() {
-        const movieCards =  this.state.moviesList.map((movie,id)=> {
-           
-           return (
-        
-            <Col className="py-2" key={id} lg={2} md={3} sm={6}>
-           <MovieThumbnail
-            id={id}
-            poster_path={movie.poster_path}
-            rate={movie.rate}
-            title={movie.title}
-            popularity={movie.popularity}
-            releaseDate={movie.releaseDate}
-            />
-            </Col>
-       
-            )
-        })
        
         return (
             <div >
-              <Row className="mx-0">
-                {movieCards}
-                </Row>
+                <MoviesGallery moviesList={this.state.moviesList} />
+                <Paginator 
+                totalPages={this.state.totalPages} 
+                currentPage={this.state.currentPage}
+                setCurrentPage={this.setCurrentPage}
+                />
             </div>
         )
     }
--- src/components/MovieThumbnail.js
+++ src/components/MovieThumbnail.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Card, CardDeck, CardGroup } from 'react-bootstrap';
+import { Card, CardDeck } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+
 
 class MovieThumbnail extends React.Component{
     constructor(props){
@@ -14,17 +15,19 @@ class MovieThumbnail extends React.Component{
                 return(
                     <div className='p-movies-page'>
                         <CardDeck>
-                       <Card text-center >
+                       <Card className="text-center"  >
                             <Link to={`/movies/${this.props.id}`}>
                             <Card.Img variant="top" src={this.props.poster_path} />
                             </Link>    
-                            {/* <div class="card-img-overlay"> */}                   
-                           
-                            {/* </div> */}
-                            <h5>{this.props.title}</h5>  
-                            <div> popularity:{this.props.popularity}</div>
-                            <div>year: {yearMovie}</div>
-                            <div className="badge badge-light">rate: {this.props.rate}</div>
+                            <div className="card-img-overlay">     
+                                <div className="position-badge">            
+                            <div className="badge badge-light badge-right">{yearMovie}</div>
+                            <div className="badge badge-light badge-left">{this.props.rate}</div>
+                            </div>  
+                            </div>
+                            <h5 className="min-height">{this.props.title}</h5>  
+                            {/* <div> popularity:{this.props.popularity}</div> */}
+                            
                           
                     </Card>
                     </CardDeck>
